a1b534750e4c567836ae3fb6c29a226ae4c8c2f5
unknown <abhinavvarma46@gmail.com>
2025-11-20 21:10:58 +0530

    name

---
+++ FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram {
+    public static void main(String[] myArgument) {
+        System.out.println("Hello World");
+        System.out.println("My name is "+myArgument[0]);
+        System.out.println("I am "+myArgument[1]+" year old");
+    }
+}
+
